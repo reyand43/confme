f4b67ead7401f6430a5ec3a9a432b77f74830597
Vlad <vladikzh@mail.ru>
2020-09-06 23:48:53 +0300

    Change scroll

--- src/containers/pages/timetable/Timetable.js
+++ src/containers/pages/timetable/Timetable.js
@@ -13,11 +13,7 @@ class Timetable extends Component {
 
   render() {
     let current = this.props.activeDate;
-<<<<<<< HEAD
-    
-=======
     //Массив расписаний, где каждый элемент JSX объект который надо отобразить
->>>>>>> 9fec2aab24f965529666f95f46bb95aff1c04fee
 
     //-----------------------------------
     const eventCard1 = (
@@ -44,11 +40,6 @@ class Timetable extends Component {
       </div>
     );
     //-----------------------------------
-<<<<<<< HEAD
-    //Массив расписаний, где каждый элемент JSX объект с событиями определенного дня
-=======
-
->>>>>>> 9fec2aab24f965529666f95f46bb95aff1c04fee
     const plans = [eventCard1, eventCard2, eventCard3];
 
     return (
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,17 +1,10 @@
 import authReducer from './auth'
 import timetableReducer from './timetable'
-<<<<<<< HEAD
-=======
 import editProfileReducer from './editProfile'
->>>>>>> 9fec2aab24f965529666f95f46bb95aff1c04fee
 import { combineReducers } from 'redux'
 
 export default combineReducers({
     auth: authReducer,
-<<<<<<< HEAD
-    timetable: timetableReducer
-=======
     timetable: timetableReducer,
     editProfile: editProfileReducer
->>>>>>> 9fec2aab24f965529666f95f46bb95aff1c04fee
 })
