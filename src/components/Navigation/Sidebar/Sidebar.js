c6ac00b3f968aee0c29c2cf1278e942231965cef
Vlad <vladikzh@mail.ru>
2020-09-07 22:56:00 +0300

    fix bugs

--- src/components/Navigation/Sidebar/Sidebar.js
+++ src/components/Navigation/Sidebar/Sidebar.js
@@ -6,7 +6,6 @@ import { connect } from "react-redux";
 import { changeHover } from "../../../store/actions/sidebar";
 
 class Sidebar extends React.Component {
-
   renderLinks(links) {
     return links.map((link, index) => {
       return (
@@ -27,37 +26,72 @@ class Sidebar extends React.Component {
   render() {
     let labels = ["", "", "", "", ""]
     if(this.props.hover){
-      labels = ["Новости", "Сообщения", "Материалы", "Расписание", "Войти"]
+      labels = (["Новости", "Сообщения", "Материалы", "Расписание", "Войти"])
+
     }
     const links = [
-      { to: "/feed", label: labels[0], icon: <i class="fa fa-newspaper-o" aria-hidden="true"></i>, exact: true },
-      { to: "/messages", label: labels[1], icon: <i class="fa fa-newspaper-o" aria-hidden="true"></i>, exact: false },
-      { to: "/materials", label: labels[2], icon: <i class="fa fa-newspaper-o" aria-hidden="true"></i>, exact: false },
-      { to: "/timetable", label: labels[3], icon: <i class="fa fa-newspaper-o" aria-hidden="true"></i>, exact: false },
+      {
+        to: "/feed",
+        label: labels[0],
+        icon: <i className="fa fa-newspaper-o" aria-hidden="true"></i>,
+        exact: true,
+      },
+      {
+        to: "/messages",
+        label: labels[1],
+        icon: <i className="fa fa-newspaper-o" aria-hidden="true"></i>,
+        exact: false,
+      },
+      {
+        to: "/materials",
+        label: labels[2],
+        icon: <i className="fa fa-newspaper-o" aria-hidden="true"></i>,
+        exact: false,
+      },
+      {
+        to: "/timetable",
+        label: labels[3],
+        icon: <i className="fa fa-newspaper-o" aria-hidden="true"></i>,
+        exact: false,
+      },
     ];
     if (!this.props.isAuthenticated) {
-      links.push({ to: "/", label: labels[4], icon: <i class="fa fa-newspaper-o" aria-hidden="true"></i>, exact: true });
+      links.push({
+        to: "/",
+        label: labels[4],
+        icon: <i className="fa fa-newspaper-o" aria-hidden="true"></i>,
+        exact: true,
+      });
     }
-    links.splice(0, 1, { to: "/feed", label: labels[0], icon: <i class="fa fa-newspaper-o" aria-hidden="true"></i>,  exact: false });
+    links.splice(0, 1, {
+      to: "/feed",
+      label: labels[0],
+      icon: <i className="fa fa-newspaper-o" aria-hidden="true"></i>,
+      exact: false,
+    });
     return (
-      <nav className={classes.Sidebar} onMouseEnter = {this.props.changeHover} onMouseLeave = {this.props.changeHover}>
-        <Logo isOpen = {this.props.hover}/>
-        <ul className="">{this.renderLinks(links)}</ul>
-      </nav>
+        <nav
+          className={classes.Sidebar}
+          onMouseEnter={this.props.changeHover}
+          onMouseLeave={this.props.changeHover}
+        >
+          <Logo isOpen={this.props.hover} />
+          <ul className="">{this.renderLinks(links)}</ul>
+        </nav>
     );
   }
 }
 
 function mapStateToProps(state) {
   return {
-    hover: state.sidebar.hover
-  }
+    hover: state.sidebar.hover,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    changeHover: () => dispatch(changeHover())
-  }
+    changeHover: () => dispatch(changeHover()),
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
